1302948704b6f7ee2f7acc4c724613a30e6dc397
Janet Webster <twixmixy@gmail.com>
2023-01-12 12:47:08 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var welcome = function (name) {
+  console.log(`Welcome, ${name}. Have a great day!`);
+};
+
+welcome("Vera"); // Welcome, Vera. Have a great day!
+welcome("Adrian"); // Welcome, Adrian. Have a great day!
+welcome("Aida"); // Welcome, Aida. Have a great day!
+
+var addTogether = function (num1, num2) {
+  return num1 + num2;
+};
+
+alert(addTogether(36, -2.88));
+console.log(addTogether(13, 72));
+var lunchForTwo = addTogether(24.56, 18.99);
